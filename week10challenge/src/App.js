580612093761904260b8d7af95d42a41e9e7b32d
Miguel Hernandez <miguelhernandez@MacBook-Pro.local>
2017-08-25 16:24:05 -0400

    very hard commit on master branch

--- week10challenge/src/App.js
+++ week10challenge/src/App.js
@@ -6,52 +6,54 @@ import Head from "./Head";
 class App extends Component {
   constructor(){
     super();
+    this.statusChange = this.statusChange.bind(this);
     this.state = {
       contactArray : [
         {
           fname: "Mark",
           lname: "Kroh",
-          phone: "666-666-6666"
+          phone: "666-666-6666",
+          status: false
         },
         {
           fname: "Angel",
           lname: "Lopez",
-          phone: "111-222-3333"
+          phone: "111-222-3333",
+          status: false
         },
         {
           fname: "Mickey",
           lname: "Hernandez",
-          phone: "000-000-1337"
+          phone: "000-000-1337",
+          status: false
         }
       ]
     }
   }
+
+  statusChange(event){
+    var index = event.target.value;
+    var newContacts = this.state.contactArray.slice();
+    newContacts[index].status = !newContacts[index].status;
+    this.setState({
+      contactArray: newContacts
+    })
+  }
+
   render() {
-
-      var contactArray = [
-        {
-          fname: "Mark",
-          lname: "Kroh",
-          phone: "666-666-6666"
-        },
-        {
-          fname: "Angel",
-          lname: "Lopez",
-          phone: "111-222-3333"
-        },
-        {
-          fname: "Mickey",
-          lname: "Hernandez",
-          phone: "000-000-1337"
-        }
-      ];
-
-    var ContactLister = contactArray.map(function(person){
-      return (
-        <Persons fname={person.fname} lname={person.lname} phone={person.phone} />
+    var ContactLister = [];
+    for (var i in this.state.contactArray){
+      ContactLister.push(
+        <Persons
+        value={i}
+        fname={this.state.contactArray[i].fname}
+        lname={this.state.contactArray[i].lname}
+        phone={this.state.contactArray[i].phone}
+        statusChange={this.statusChange}
+        colorValue={this.state.contactArray[i].status}
+        />
       )
     }
-  )
 
     return (
       <div className="App">
@@ -65,8 +67,5 @@ class App extends Component {
 export default App;
 
 
-// VERY EASY: Create a React class component that renders "hello world" in an h1 tag. You don't need props for this one.
-// EASY: Pass a prop for "name" into the component you just created so the h1 tag reads, "Hello world, {name}"
-// MEDIUM: Create a child Contact component inside the original component you created that you pass in a firstName, lastName, and phoneNumber and print out "You need to contact {firstName} {lastName} at {phoneNumber}
-// HARD: Using an array of contacts, loop through each one and create a new Contact component for each
+
 // VERY HARD: In your list of Contacts, have a status icon by each one that starts as orange "Pending" status. Then have a call button that when clicked, changes the state to a green "Done" status.
--- week10challenge/src/Persons.js
+++ week10challenge/src/Persons.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 function Persons(props){
+  var color = (props.colorValue) ? "green" : "orange";
   return (
-    <p>You need to contact {props.fname} {props.lname} at {props.phone}</p>
+    <div className="Contact">
+      <div className="status" style={{'background-color': color}}>
+      </div>
+      <p>You need to contact {props.fname} {props.lname} at {props.phone}</p>
+      <button id="myButton" onClick={props.statusChange} value={props.value}>Done</button>
+    </div>
   )
 }
 
