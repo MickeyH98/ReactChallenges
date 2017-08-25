21120bd880f78e8f7d19c276d7fdc71f9163f3be
Miguel Hernandez <miguelhernandez@MacBook-Pro.local>
2017-08-25 15:26:27 -0400

    hard commit

--- week10challenge/src/App.js
+++ week10challenge/src/App.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
-import Head from "./Head";
 import Persons from "./Persons";
+import Head from "./Head";
 
 class App extends Component {
-
+  constructor(){
+    super();
+    this.state = {
+      contactArray : [
+        {
+          fname: "Mark",
+          lname: "Kroh",
+          phone: "666-666-6666"
+        },
+        {
+          fname: "Angel",
+          lname: "Lopez",
+          phone: "111-222-3333"
+        },
+        {
+          fname: "Mickey",
+          lname: "Hernandez",
+          phone: "000-000-1337"
+        }
+      ]
+    }
+  }
   render() {
+
+    var ContactLister = this.state.contactArray.map(function(person){
+      return (
+        <Persons fname={person.fname} lname={person.lname} phone={person.phone} />
+      )
+    }
+  )
+
     return (
       <div className="App">
         <Head name="Mickey"/>
-        <Persons fname="Mickey" lname="Hernandez" phone="123-123-1234" />
+        {ContactLister}
       </div>
     );
   }
@@ -17,6 +46,4 @@ class App extends Component {
 
 export default App;
 
-
-
-// MEDIUM: Create a child Contact component inside the original component you created that you pass in a firstName, lastName, and phoneNumber and print out "You need to contact {firstName} {lastName} at {phoneNumber}
+//HARD: Using an array of contacts, loop through each one and create a new Contact component for each.
